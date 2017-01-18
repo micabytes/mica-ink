8133be00ffda5350ee4c89d1e21214b745e9d591
micabytes <michael.akinde@micabytes.com>
2017-01-18 23:28:37 +0100

    Fixed tests for Knots, Glue and Diverts.

--- src/test/kotlin/com/micabytes/ink/DivertSpec.kt
+++ src/test/kotlin/com/micabytes/ink/DivertSpec.kt
@@ -51,6 +51,17 @@ class DivertSpec : WordSpec() {
         |=== paragraph_2 ===
         |You open the gate, and step out onto the path.
       """.trimMargin()
+
+      "branch directly from choices" {
+        val inputStream = IOUtils.toInputStream(divertOnChoice, "UTF-8")
+        val story = InkParser.parse(inputStream, TestWrapper(), "Test")
+        story.next()
+        story.choose(0)
+        val text = story.next()
+        text.size shouldBe(2)
+        text.get(1) shouldBe("You open the gate, and step out onto the path.")
+      }
+
       val complexBranching =
           """=== back_in_london ===
         |
@@ -79,55 +90,41 @@ class DivertSpec : WordSpec() {
         |<> as fast as we could.
       """.trimMargin()
 
-
-
-      /*
-      "- branch directly from choices" {
-        val inputStream = IOUtils.toInputStream(divertOnChoice, "UTF-8")
-        val story = InkParser.parse(inputStream, TestWrapper(), "Test")
-        story.next()
-        story.choose(0)
-        val text = story.next()
-        text.size() shouldBe(1)
-        text.get(0) shouldBe("You open the gate, and step out onto the path.")
-      }
-
-      "- be usable to branch and join text seamlessly (example 1)" {
+      "be usable to branch and join text seamlessly (example 1)" {
         // First path through text
         val inputStream = IOUtils.toInputStream(complexBranching, "UTF-8")
         val story = InkParser.parse(inputStream, TestWrapper(), "Test")
         story.next()
         story.choose(0)
         val text = story.next()
-        text.size() shouldBe(2)
-        text.get(0) shouldBe("\"There is not a moment to lose!\" I declared.")
-        text.get(1) shouldBe("We hurried home to Savile Row as fast as we could.")
+        text.size shouldBe(3)
+        text.get(1) shouldBe("\"There is not a moment to lose!\" I declared.")
+        text.get(2) shouldBe("We hurried home to Savile Row as fast as we could.")
       }
 
-      "- be usable to branch and join text seamlessly (example 2)" {
+      "be usable to branch and join text seamlessly (example 2)" {
         // Second path through text
         val inputStream = IOUtils.toInputStream(complexBranching, "UTF-8")
         val story = InkParser.parse(inputStream, TestWrapper(), "Test")
         story.next()
         story.choose(1)
         val text = story.next()
-        text.size() shouldBe(3)
-        text.get(0) shouldBe("\"Monsieur, let us savour this moment!\" I declared.")
-        text.get(1) shouldBe("My master clouted me firmly around the head and dragged me out of the door.")
-        text.get(2) shouldBe("He insisted that we hurried home to Savile Row as fast as we could.")
+        text.size shouldBe(4)
+        text.get(1) shouldBe("\"Monsieur, let us savour this moment!\" I declared.")
+        text.get(2) shouldBe("My master clouted me firmly around the head and dragged me out of the door.")
+        text.get(3) shouldBe("He insisted that we hurried home to Savile Row as fast as we could.")
       }
 
-      "- be usable to branch and join text seamlessly (example 3)" {
+      "be usable to branch and join text seamlessly (example 3)" {
         // Third path through text
         val inputStream = IOUtils.toInputStream(complexBranching, "UTF-8")
         val story = InkParser.parse(inputStream, TestWrapper(), "Test")
         story.next()
         story.choose(2)
         val text = story.next()
-        text.size() shouldBe(1)
-        text.get(0) shouldBe("We hurried home to Savile Row as fast as we could.")
+        text.size shouldBe(2)
+        text.get(1) shouldBe("We hurried home to Savile Row as fast as we could.")
       }
-      */
 
     }
   }
